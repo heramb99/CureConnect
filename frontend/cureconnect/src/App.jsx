4cd1537b0db6a00ea2958dd395483b314eff5f48
Heramb Kulkarni <hr835429@dal.ca>
2024-04-04 00:23:29 -0300

    Added redirects file

--- frontend/cureconnect/src/App.jsx
+++ frontend/cureconnect/src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing/Landing';
 import Login from './pages/LoginSignup/Login';
 import Register from './pages/LoginSignup/Register';
